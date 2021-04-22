fe608844e0a8f9985f8cb145a083587531ee0c0a
lelipwn <99sergi@gmail.com>
2021-04-22 18:10:10 +0200

    Estructura segun el diagrama de clases basico

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ import pygame_widgets
 import random
 from Constantes import *
 
+# Inizializar pantalla
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('alpha')
+
+
+def renderWindow():
+    screen.fill((0, 0, 125))
+    pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
+
+    # main loop
     run = True
     while run:
-        for event in pygame.event.get():
+        clock.tick(30)
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 run = False
-        screen
+        renderWindow()
     pygame.quit()
 
-
-# Inizializar pantalla
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 if __name__ == "__main__":
     main()
