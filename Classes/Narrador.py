3102e53d3b75a0ce7282ea4fac9563d4cc56e194
Luca <Luca.monti1997@gmail.com>
2021-04-22 17:39:45 +0200

    Un par de funciones en Narrador

--- Classes/Narrador.py
+++ Classes/Narrador.py
@@ -7,11 +7,17 @@ class Narrador(object):
         Se encarga de "dirigir" la partida. Reparte las cartas, gestiona los turnos, generación de recursos, eventos
         aleatorios, etc.
         """
-        self.player1_turn = random.choice([True, False])
-        self.player2_turn = not self.player1_turn
+        self.turno_jugador1 = False
+        self.turno_jugador2 = False
+        self.turno_acabado = False
+        self.jugadores = ''
+
+    def IniciarPartida(self):
+        self.turno_jugador1 = random.choice([True, False])
+        self.turno_jugador2 = not self.turno_jugador1
         self.turno_acabado = True  # Indica si el narrador quiere cambiar turno
 
     def CambiarTurno(self):
-        self.player1_turn = not self.player1_turn
-        self.player2_turn = not self.player1_turn
+        self.turno_jugador1 = not self.turno_jugador1
+        self.turno_jugador2 = not self.turno_jugador1
         self.turno_acabado = False
