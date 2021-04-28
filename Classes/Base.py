a765ab33299be375f374c234a2b904dfab3d954c
Luca <Luca.monti1997@gmail.com>
2021-04-28 18:44:05 +0200

    Clase Base cosas

--- Classes/Base.py
+++ Classes/Base.py
@@ -4,20 +4,33 @@ import pygame
 class Base(object):
 
     def __init__(self, coordenadas, dimensiones):
-        self.coordenadas = coordenadas
-        self.dimensiones = dimensiones
+        self.coord = coordenadas
+        self.dimen = dimensiones
         self.imagenesbase = {}
 
     def InicizializarBase(self):
         self.imagenesbase = {
-            "torre": pygame.image.load("Assets/TestAssets/Castillo1/TorreSencilla1.png").convert_alpha(),
+            "torre_central": pygame.image.load("Assets/TestAssets/Castillo1/TorreSencilla1.png").convert_alpha(),
+            "torre_lateral": pygame.image.load("Assets/TestAssets/Castillo1/TorreSencilla1.png").convert_alpha(),
             "muralla": pygame.image.load("Assets/TestAssets/Castillo1/Muralla1.png").convert_alpha()
-            }
-
-        for key in self.imagenesbase:
-            self.imagenesbase[key] = pygame.transform.smoothscale(self.imagenesbase[key], (
-                int(self.imagenesbase[key].get_width() * 2), int(self.imagenesbase[key].get_height() * 2)))
+        }
+        self.imagenesbase["torre_central"] = pygame.transform.smoothscale(self.imagenesbase["torre_central"],
+                                                                          (int(self.imagenesbase[
+                                                                                   "torre_central"].get_width() * 1.2),
+                                                                           int(self.imagenesbase[
+                                                                                   "torre_central"].get_height() * 1.2)))
+        self.imagenesbase["muralla"] = pygame.transform.smoothscale(self.imagenesbase["muralla"],
+                                                                    (
+                                                                        int(self.imagenesbase[
+                                                                                "muralla"].get_width() * 0.8),
+                                                                        int(self.imagenesbase[
+                                                                                "muralla"].get_height() * 0.8)))
 
     def Dibujar(self, pantalla):
-        pantalla.blit(self.imagenesbase["torre"], [200, 200])
-        pantalla.blit(self.imagenesbase["muralla"], [200, 200])
+        # Torre central
+
+        pantalla.blit(self.imagenesbase["torre_central"], [int(self.coord[0] + self.dimen[0] / 2), 200])
+        # Torre izquierda
+        pantalla.blit(self.imagenesbase["torre_lateral"], [self.coord[0], 200])
+        pantalla.blit(self.imagenesbase["muralla"],
+                      [int(self.coord[0] + self.dimen[0] / 2), 200])
